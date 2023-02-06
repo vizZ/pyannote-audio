5934196bd26da44fc4c5a0290cb5b5e6ca81517e
Artur Glier <arturglier@gmail.com>
2023-02-07 00:17:07 +0100

    Update io.py
    Removed explicit declaration of torchaudio audio backend

--- pyannote/audio/core/io.py
+++ pyannote/audio/core/io.py
@@ -39,8 +39,6 @@ import torchaudio
 from pyannote.core import Segment
 from torch import Tensor
 
-torchaudio.set_audio_backend("sox_io")
-
 AudioFile = Union[Text, Path, IOBase, Mapping]
 
 AudioFileDocString = """
